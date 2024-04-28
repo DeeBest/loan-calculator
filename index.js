d44d3789b0eb40d7bf2360ef34809e4256dba85e
DeeBest <simphiwedladla8@gmail.com>
2024-04-28 19:49:15 +0200

    added function to calculate the loan

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const currentYear = new Date().getFullYear();
 const footerParagraph = document.getElementById('footer-paragraph');
 footerParagraph.textContent = `Created by \xA9Simphiwe${currentYear}.`;
+
+function calculateLoan() {
+  const loanAmount = document.getElementById('loan-amount').value;
+  const interestRate = document.getElementById('interest-rate').value;
+  const duration = document.getElementById('duration').value;
+  const monthlyPaymentOutput = document.getElementById(
+    'monthly-payment-output'
+  );
+
+  let numberLoanAmount = Number(loanAmount);
+  let numberInterestRate = Number(interestRate);
+  let numberDuration = Number(duration);
+
+  let interest =
+    (numberLoanAmount * (numberInterestRate * 0.01)) / numberDuration;
+
+  let monthlyInstallment = (
+    numberLoanAmount / numberDuration +
+    interest
+  ).toFixed(2);
+
+  monthlyPaymentOutput.textContent = '$' + monthlyInstallment;
+}
